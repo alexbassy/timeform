61a43fab9257a44ae5e9fe9594d513aae05f8340
Alex Bass <alexpjbass@gmail.com>
2018-07-04 11:29:33 +0200

    feat: getForms endpoint, will also add schedule info

--- routes/forms.js
+++ routes/forms.js
@@ -5,8 +5,22 @@ const jwt = require('../lib/jwt')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const forms = await getForms({ token: req.user.token })
-  return res.json(forms)
+  const response = await getForms({ token: req.user.token })
+
+  const formsWithFlattenedFields = response.items.map(form => ({
+    id: form.id,
+    title: form.title,
+    lastUpdated: form.last_updated_at,
+    isPublic: form.settings.is_public,
+    isTrial: form.settings.is_trial,
+    url: form.self.href
+  }))
+
+  return res.json({
+    count: response.total_items,
+    pages: response.page_count,
+    items: formsWithFlattenedFields
+  })
 })
 
 router.get('/:id', async (req, res) => {
