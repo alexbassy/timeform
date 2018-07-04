9e400be3a6c8a8102a79d04e45ad5ace384ca8ff
Alex Bass <alexpjbass@gmail.com>
2018-07-04 11:29:43 +0200

    feat: list forms in frontend

--- public/scripts/Scheduler.jsx
+++ public/scripts/Scheduler.jsx
@@ -1,9 +1,100 @@
 import React, { Component } from 'react'
+import styled from 'react-emotion'
+import WithForms from './WithForms'
+
+const icon = {
+  public: `<svg xmlns="http://www.w3.org/2000/svg" width="768" height="768" viewBox="0 0 768 768"><g/><path d="M384 288c52.5 0 96 43.5 96 96s-43.5 96-96 96-96-43.5-96-96 43.5-96 96-96zm0 256.5c88.5 0 160.5-72 160.5-160.5s-72-160.5-160.5-160.5-160.5 72-160.5 160.5 72 160.5 160.5 160.5zm0-400.5c160.5 0 297 99 352.5 240C681 525 544.5 624 384 624S87 525 31.5 384C87 243 223.5 144 384 144z"/></svg>`,
+  private: `<svg xmlns="http://www.w3.org/2000/svg" width="768" height="768" viewBox="0 0 768 768"><g/><path d="M379.5 288h4.5c52.5 0 96 43.5 96 96v6zm-138 25.5c-10.5 21-18 45-18 70.5 0 88.5 72 160.5 160.5 160.5 25.5 0 49.5-7.5 70.5-18L405 477c-6 1.5-13.5 3-21 3-52.5 0-96-43.5-96-96 0-7.5 1.5-15 3-21zm-177-177L105 96l567 567-40.5 40.5c-35.98-35.52-72.2-70.8-108-106.5-43.5 18-90 27-139.5 27-160.5 0-297-99-352.5-240C57 321 99 267 151.5 225c-29.26-29.24-57.98-59.02-87-88.5zm319.5 87c-21 0-40.5 4.5-58.5 12l-69-69c39-15 82.5-22.5 127.5-22.5 160.5 0 295.5 99 351 240-24 60-61.5 111-109.5 151.5l-93-93c7.5-18 12-37.5 12-58.5 0-88.5-72-160.5-160.5-160.5z"/></svg>`
+}
+
+const Container = styled('div')`
+  display: flex;
+  border-top: 1px solid #eee;
+  margin-top: 16px;
+`
+
+const FormsList = styled('ul')`
+  flex: 1;
+  max-width: 300px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
+
+const FormItemWrap = styled('li')`
+  padding: 12px 8px 12px 0;
+  border-bottom: 1px solid var(--schedulerBorderColor);
+`
+
+const FormItemTitle = styled('div')`
+  font-size: 16px;
+  max-width: calc(100% - 8px);
+  margin-bottom: 8px;
+  font-weight: 500;
+  text-overflow: ellipsis;
+  white-space: pre;
+  overflow: hidden;
+`
+
+const VisibilityStatus = styled('div')`
+  display: flex;
+  font-size: 14px;
+`
+
+const VisibilityIcon = styled('img')`
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
+`
+
+const FormItem = ({ form, onClick }) => {
+  const { title, isPublic } = form
+  return (
+    <FormItemWrap
+      onClick={() => onClick(form)}
+    >
+      <FormItemTitle>
+        {title}
+      </FormItemTitle>
+      <VisibilityStatus>
+        <VisibilityIcon src={`data:image/svg+xml;utf8,${isPublic ? icon.public : icon.private }`} />
+        {isPublic ? 'Public' : 'Private'}
+      </VisibilityStatus>
+    </FormItemWrap>
+  )
+}
+
+const ScheduleView = styled('div')`
+  flex: 1;
+  border-left: 1px solid var(--schedulerBorderColor);
+`
 
 class Scheduler extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selectedForm: null
+    }
+  }
+
   render () {
     return (
-      <div>Scheduler</div>
+      <WithForms>
+        {forms => (
+          <Container>
+            <FormsList>
+              {forms.items.map(form => (
+                <FormItem
+                  key={form.id}
+                  form={form}
+                  onClick={() => this.setState({ selectedForm: form })}
+                />
+              ))}
+            </FormsList>
+            <ScheduleView form={this.state.selectedForm} />
+          </Container>
+        )}
+      </WithForms>
     )
   }
 }
---
+++ public/scripts/WithForms.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { getForms } from './api-lib'
+
+const cached = {"count":19,"pages":1,"items":[{"id":"nh7r3S","title":"Standup record keeper","lastUpdated":"2018-06-28T08:54:07+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/nh7r3S"},{"id":"PFJBMy","title":"Form with other field","lastUpdated":"2018-06-27T15:38:59+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/PFJBMy"},{"id":"HEpG1a","title":"How helpful was this?","lastUpdated":"2018-06-15T16:57:48+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/HEpG1a"},{"id":"T2ZfMy","title":"Customer Feedback","lastUpdated":"2018-06-07T08:31:09+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/T2ZfMy"},{"id":"M72WRC","title":"Very short German form with HubSpot Integration","lastUpdated":"2018-06-04T08:58:20+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/M72WRC"},{"id":"Af5xJz","title":"Team Breakfasts","lastUpdated":"2018-04-27T09:07:42+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/Af5xJz"},{"id":"CzydoC","title":"GDPR (email for advanced)","lastUpdated":"2018-03-22T11:53:34+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/CzydoC"},{"id":"ijjGIk","title":"GDPR (email for intermediate )","lastUpdated":"2018-03-22T11:48:02+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/ijjGIk"},{"id":"kUeGNf","title":"GDPR (email for beginner only)","lastUpdated":"2018-03-22T11:47:04+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/kUeGNf"},{"id":"xKSYoc","title":"GDPR","lastUpdated":"2018-03-22T16:35:12+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/xKSYoc"},{"id":"jK0vqb","title":"A lot of responses","lastUpdated":"2018-03-19T18:44:12+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/jK0vqb"},{"id":"Bm3ly8","title":"Generic contact form","lastUpdated":"2018-06-10T16:32:25+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/Bm3ly8"},{"id":"ZEbL2o","title":"To-do","lastUpdated":"2018-03-20T11:27:31+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/ZEbL2o"},{"id":"gV0NkV","title":"All Fields","lastUpdated":"2018-02-07T13:17:33+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/gV0NkV"},{"id":"bWZ4Ie","title":"Frontend Questionnaire","lastUpdated":"2017-11-17T10:30:13+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/bWZ4Ie"},{"id":"Zzcp0A","title":"Team building ideas","lastUpdated":"2018-02-11T13:57:55+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/Zzcp0A"},{"id":"XR2aKM","title":"Offsite","lastUpdated":"2017-11-06T20:21:38+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/XR2aKM"},{"id":"JiGbYW","title":"Discover Project Survey","lastUpdated":"2017-08-28T09:21:43+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/JiGbYW"},{"id":"Lk8K65","title":"Intro Typeform","lastUpdated":"2018-03-23T17:23:56+00:00","isPublic":true,"isTrial":false,"url":"https://api.typeform.com/forms/Lk8K65"}]}
+
+class WithForms extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  async componentDidMount () {
+    // const forms = await getForms()
+    setTimeout(() => this.setState({ forms: cached }), 100)
+    console.log(cached)
+  }
+
+  render () {
+    if (this.state.forms) {
+      return this.props.children(this.state.forms)
+    }
+
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+}
+
+export default WithForms
