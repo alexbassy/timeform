acde3f5711703ac5566c8793e6fe49d608e79ef8
Alex Bass <alexpjbass@gmail.com>
2018-07-13 16:36:51 +0200

    feat: add rule to form definition, add route for creating rule

--- routes/forms.js
+++ routes/forms.js
@@ -1,5 +1,6 @@
+const http = require('http')
 const express = require('express')
-const { Account } = require('../services/db')
+const { Account, Rule } = require('../services/db')
 const { TF_BASE_URL, getForms, getForm, changeFormPublicState } = require('../services/typeform-api')
 const jwt = require('../lib/jwt')
 const router = express.Router()
@@ -16,10 +17,22 @@ router.get('/', async (req, res) => {
     url: form.self.href
   }))
 
+  const formIDs = formsWithFlattenedFields.map(form => form.id)
+
+  const rules = await Rule.find({ formId: { $in: formIDs } })
+
+  const formsWithRules = formsWithFlattenedFields.map(form => {
+    const matching = rules.find(rule => rule.formId === form.id)
+    if (matching) {
+      console.log(matching)
+    }
+    return Object.assign({}, form, { rule: matching || {} })
+  })
+
   return res.json({
     count: response.total_items,
     pages: response.page_count,
-    items: formsWithFlattenedFields
+    items: formsWithRules
   })
 })
 
@@ -28,5 +41,24 @@ router.get('/:id', async (req, res) => {
   return res.json(form)
 })
 
+router.post('/rule/:id', async (req, res) => {
+  const accountId = req.user._id
+  const { frequency, condition, begin, end } = req.body
+  const rule = new Rule({
+    formId: req.params.id,
+    accountId: accountId,
+    enabled: true,
+    condition: condition,
+    recurring: frequency,
+    begin: begin,
+    end: end
+  })
+  rule.save(err => {
+    if (err) {
+      return res.status(400).json(err)
+    }
+    res.status(201).json(rule)
+  })
+})
 
 module.exports = router
